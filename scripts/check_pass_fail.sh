ed58ad0d2fa811e07d72019f16f96e597db8db47
Stephen Kelly <stevekm@users.noreply.github.com>
2021-10-14 10:57:26 -0400

    update script notes

--- scripts/check_pass_fail.sh
+++ scripts/check_pass_fail.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Script to quickly see how many runs passed and failed
+
+# usages
 # $ ./check_pass_fail.sh | cut -f1-2 | sort | uniq -c
+# for i in $(./check_pass_fail.sh | grep fail | cut -f3); do echo $i; rm -rf "${i}/work"; rm -rf "${i}/tmp"; done
+# for i in $(./check_pass_fail.sh | grep success | grep -v keep | cut -f3); do echo $i; rm -rf "${i}/work" "${i}/tmp" "${i}/output"; done
 
 for i in $(find . -maxdepth 2 -type f -name toil.log); do
     run_dir=$(basename $(dirname $i))
